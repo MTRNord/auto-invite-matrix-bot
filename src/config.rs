69949e69ec0f6852b4f1f2b1927ca80539b71125
Marcel <mtrnord1@gmail.com>
2020-02-17 18:55:36 +0100

    Initial commit
    Took 1 hour 59 minutes

---
+++ src/config.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io::BufReader;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Config {
+    pub message: String,
+    pub servers: Vec<Homeserver>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Homeserver {
+    pub address: String,
+    pub mxid: String,
+    pub access_token: Option<String>,
+    pub password: Option<String>,
+}
+
+
+pub fn load_config(config_file: String) -> Result<Config, serde_yaml::Error> {
+    let f = File::open(config_file).expect("Unable to open config file");
+    let br = BufReader::new(f);
+
+    let deserialized_config: Config = serde_yaml::from_reader(br)?;
+    Ok(deserialized_config)
+}
---
+++ src/main.rs
@@ -0,0 +1,108 @@
+use std::convert::TryFrom;
+
+use clap::Clap;
+use futures_util::future::join_all;
+use futures_util::stream::TryStreamExt as _;
+use ruma_client::{
+    self,
+    events::{
+        EventType,
+        room::message::{MessageEventContent, NoticeMessageEventContent},
+    },
+    HttpsClient, Session,
+};
+use ruma_client::api::r0::membership::invite_user;
+use ruma_client::api::r0::membership::join_room_by_id;
+use ruma_client::api::r0::message::create_message_event;
+use ruma_client::api::r0::sync::sync_events::IncomingResponse;
+use ruma_client::identifiers::UserId;
+use url::Url;
+use crate::config::{load_config, Config, Homeserver};
+
+mod config;
+
+async fn do_stuff(
+    config: &Config,
+    server: &Homeserver,
+) -> Result<(), ruma_client::Error> {
+    let session = Session {
+        device_id: "Ruma Bot".to_string(),
+        access_token: server.access_token.as_ref().expect("missing access_token").to_owned(),
+        user_id: UserId::try_from(server.mxid.as_str()).unwrap(),
+    };
+
+    let client = HttpsClient::https(Url::parse(server.address.as_str()).expect("unable to parse url"), Some(session));
+    //client.log_in(username, password, None, None).await?;
+
+    let mut sync_stream = Box::pin(client.sync(None, None, false));
+    let message = config.message.clone();
+    while let Some(response) = sync_stream.try_next().await? {
+        let res: IncomingResponse = response;
+        for (room_id, _room) in res.rooms.invite {
+            // Auto join rooms
+            println!("Invited to {:?}", room_id.clone());
+            client
+                .request(join_room_by_id::Request {
+                    room_id: room_id.clone(),
+                    third_party_signed: None,
+                })
+                .await?;
+            println!("Joined {:?}", room_id.clone());
+            let response = client
+                .request(invite_user::Request {
+                    room_id: room_id.clone(),
+                    user_id: UserId::try_from("@MTRNord:matrix.ffslfl.net").unwrap(),
+                })
+                .await?;
+            println!("Invited correct user {:?}", response);
+            client
+                .request(create_message_event::Request {
+                    room_id,
+                    event_type: EventType::RoomMessage,
+                    txn_id: "1".to_owned(),
+                    data: MessageEventContent::Notice(NoticeMessageEventContent {
+                        body: message.clone(),
+                        relates_to: None,
+                    }),
+                })
+                .await?;
+
+            println!("Sent a message about what happened");
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "MTRNord")]
+struct Opts {
+    #[clap(short = "c", long = "config", default_value = "config.yaml")]
+    config: String,
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: i32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ruma_client::Error> {
+    let opts: Opts = Opts::parse();
+
+    // TODO actually implement logger
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    let config = load_config(opts.config).expect("unable to read config");
+    let mut futures = vec![];
+
+    config.servers.iter().for_each(|x| {
+        futures.push(do_stuff(&config, x));
+    });
+
+    join_all(futures).await;
+    Ok(())
+}
